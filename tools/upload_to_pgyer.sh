401d360f92251492e9dc10b58c9082fbcf23abf5
雷子 <cjlsire@126.com>
2025-07-10 15:22:10 +0800

    更新上传脚本，修复 APK 路径引用并添加 Pgyer 上传脚本

--- tools/upload_to_pgyer.sh
+++ tools/upload_to_pgyer.sh
@@ -2,33 +2,20 @@
 
 # 上传fir
 apk_path="../build/app/outputs/flutter-apk/app-release.apk"
-fir_cli_path="./go-fir-cli"
+fir_cli_path="./pgyer_upload.sh"
+api_token=$FIR_TOKEN
+
+if [ -z "$api_token" ]; then
+  echo "请设置 FIR_TOKEN 环境变量。"
+  exit 1
+fi
 
 echo "开始上传 APK 到 Pgyer..."
-echo "APK 路径: $apk_path"
-echo "FIR CLI 路径: $fir_cli_path"
-echo "token : $FIR_TOKEN"
+"$fir_cli_path -k $api_token $apk_path"
 
-
-# if [ ! -f "$apk_path" ]; then
-#   echo "APK file not found at $apk_path"
-#   exit 1
-# fi
-
-# echo "正在登录 Pgyer..."
-# $fir_cli_path login -t $api_token
-
-# if [ $? -ne 0 ]; then
-#   echo "登录 Pgyer 失败，请检查 API Token 是否正确。"
-#   exit 1
-# fi
-
-# echo "正在上传 APK 到 Pgyer..."
-# $fir_cli_path upload -f $apk_path
-
-# if [ $? -ne 0 ]; then
-#   echo "上传 APK 到 Pgyer 失败。"
-#   exit 1
-# fi
+if [ $? -ne 0 ]; then
+  echo "上传 APK 到 Pgyer 失败。"
+  exit 1
+fi
 
 echo "APK 上传成功！"
