8613dec6ff62aa2982dc4ff4dfa65337b56b68a3
雷子 <cjlsire@126.com>
2025-07-10 15:05:43 +0800

    更新上传脚本，添加 FIR_TOKEN 环境变量以支持 Pgyer 上传

--- tools/upload_to_pgyer.sh
+++ tools/upload_to_pgyer.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
 # 上传fir
-api_token="a7d3b217e7132bd0b79ffa45f35651b3"
 apk_path="../build/app/outputs/flutter-apk/app-release.apk"
 fir_cli_path="./go-fir-cli"
 
-if [ ! -f "$apk_path" ]; then
-  echo "APK file not found at $apk_path"
-  exit 1
-fi
+echo "开始上传 APK 到 Pgyer..."
+echo "APK 路径: $apk_path"
+echo "FIR CLI 路径: $fir_cli_path"
+echo "token : $FIR_TOKEN"
 
-echo "正在登录 Pgyer..."
-$fir_cli_path login -t $api_token
 
-if [ $? -ne 0 ]; then
-  echo "登录 Pgyer 失败，请检查 API Token 是否正确。"
-  exit 1
-fi
+# if [ ! -f "$apk_path" ]; then
+#   echo "APK file not found at $apk_path"
+#   exit 1
+# fi
 
-echo "正在上传 APK 到 Pgyer..."
-$fir_cli_path upload -f $apk_path
+# echo "正在登录 Pgyer..."
+# $fir_cli_path login -t $api_token
 
-if [ $? -ne 0 ]; then
-  echo "上传 APK 到 Pgyer 失败。"
-  exit 1
-fi
+# if [ $? -ne 0 ]; then
+#   echo "登录 Pgyer 失败，请检查 API Token 是否正确。"
+#   exit 1
+# fi
+
+# echo "正在上传 APK 到 Pgyer..."
+# $fir_cli_path upload -f $apk_path
+
+# if [ $? -ne 0 ]; then
+#   echo "上传 APK 到 Pgyer 失败。"
+#   exit 1
+# fi
 
 echo "APK 上传成功！"
